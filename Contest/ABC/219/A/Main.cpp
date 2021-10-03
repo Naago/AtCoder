fc4faf5be6c11b611da53d3925f415c6eeb24c92
Naago <71188333+Naago@users.noreply.github.com>
2021-10-04 03:34:48 +0900

    答案を追加し直し。
    文字コードも"UTF-8 with BOM"にした。

---
+++ Contest/ABC/219/A/Main.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+int main()
+{
+  int x;
+  std::cin >> x;
+  
+  if (x >= 90)
+  {
+    std::cout << "expert" << std::endl;
+  }
+  else if (x >= 70)
+  {
+    std::cout << (90 - x) << std::endl;
+  }
+  else if (x >= 40)
+  {
+    std::cout << (70 - x) << std::endl;
+  }
+  else
+  {
+    std::cout << (40 - x) << std::endl;
+  }
+  return 0;
+}
