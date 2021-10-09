e921c99b97a0765e5ca36e3a922afd4a8f179cd1
Naago <71188333+Naago@users.noreply.github.com>
2021-10-09 14:44:58 +0900

    答案を追加。

---
+++ Contest/ABC/220/A/Main.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+
+int main()
+{
+	int a, b, c;
+	std::cin >> a >> b >> c;
+	// std::cout << a << ", " << b << ", " << c << std::endl;
+
+	int d = c;
+
+	while (d <= 1000)
+	{
+		if ((d >= a) && (d <= b))
+		{
+			break;
+		}
+		d += c;
+	}
+
+	if (d > 1000)
+	{
+		d = -1;
+	}
+
+	std::cout << d << std::endl;
+	return 0;
+}
