9bb3a498281b715fa57198c055bcb20327997c43
Naago <71188333+Naago@users.noreply.github.com>
2021-10-09 14:57:59 +0900

    答案を追加。

---
+++ Contest/ABC/221/A/Main.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+int main()
+{
+	int a, b;
+	std::cin >> a >> b;
+	// std::cout << a << ", " << b << std::endl;
+
+	auto n = 1;
+	for (auto i = 0; i < (a - b); i++)
+	{
+		n *= 32;
+	}
+
+	std::cout << n << std::endl;
+	return 0;
+}
