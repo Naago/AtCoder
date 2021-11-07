a7f98d4a994dcd79da9621cceb35c4235547501c
Naago <71188333+Naago@users.noreply.github.com>
2021-11-08 02:22:48 +0900

    答案を追加。

---
+++ Contest/ABC/226/A/Main.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+
+int main()
+{
+	float f;
+	std::cin >> f;
+
+	int n = static_cast<int>(f);
+
+	const int n10 = static_cast<int>(f * 10);
+	if (n10 % 10 >= 5)
+	{
+		n += 1;
+	}
+
+	std::cout << n << std::endl;
+	return 0;
+}
